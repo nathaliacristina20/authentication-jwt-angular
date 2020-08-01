f8d60c7895e390b49d80aaf4be259ad0d02bf6c9
Nathalia Cristina <nathaliacristina20@hotmail.com>
2020-07-31 23:47:45 -0300

    lazy load for main

--- packages/frontend/src/app/app-routing.module.ts
+++ packages/frontend/src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
+import { MainModule } from './main/main.module';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: '/main/people' },
+  { path: 'main', loadChildren: () => MainModule },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- packages/frontend/src/app/main/main-routing.module.ts
+++ packages/frontend/src/app/main/main-routing.module.ts
@@ -1,7 +1,13 @@
+import { ProductsComponent } from './components/products/products.component';
+import { PeopleComponent } from './components/people/people.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'people' },
+  { path: 'people', component: PeopleComponent },
+  { path: 'products', component: ProductsComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
