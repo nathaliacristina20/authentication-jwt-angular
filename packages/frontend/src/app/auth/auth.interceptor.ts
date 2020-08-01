b8470b0adb4065966fa30191fa01d92e177f2c42
Nathalia Cristina <nathaliacristina20@hotmail.com>
2020-08-01 01:45:36 -0300

    interceptor response

--- packages/frontend/src/app/auth/auth.interceptor.ts
+++ packages/frontend/src/app/auth/auth.interceptor.ts
@@ -1,23 +1,42 @@
-import { HttpInterceptor, HttpRequest, HttpHandler } from '@angular/common/http';
+import { AuthService } from './services/auth.service';
+import { HttpInterceptor, HttpRequest, HttpHandler, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { catchError } from 'rxjs/operators';
+import { throwError } from 'rxjs';
+import { Router } from '@angular/router';
 
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
 
-  constructor(){
+  constructor(
+    private authService: AuthService,
+    private router: Router
+  ){
 
   }
 
   intercept(req: HttpRequest<any>, next: HttpHandler) {
-    const tokenLocalStorage = localStorage.getItem('token');
+    const token = localStorage.getItem('token');
 
-    if (tokenLocalStorage){
+    if (token){
       const authReq = req.clone({
         setHeaders: {
-          Authorization: tokenLocalStorage
+          Authorization: 'sdds'
         }
-      })
-      return next.handle(authReq);
+      });
+
+      return next.handle(authReq)
+      .pipe(
+        catchError((error) => {
+          if (error instanceof HttpErrorResponse){
+            if (error.status === 401){
+              this.authService.logout();
+              this.router.navigateByUrl('/auth/login');
+            }
+          }
+          return throwError(error);
+        })
+      )
     }
     return next.handle(req);
   }
