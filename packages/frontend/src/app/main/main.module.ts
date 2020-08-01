fa73983b6e340a740b171d46ad50235d491202e4
Nathalia Cristina <nathaliacristina20@hotmail.com>
2020-07-31 23:21:47 -0300

    create people, products component

--- packages/frontend/src/app/main/main.module.ts
+++ packages/frontend/src/app/main/main.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { MainRoutingModule } from './main-routing.module';
-
+import { PeopleComponent } from './components/people/people.component';
+import { ProductsComponent } from './components/products/products.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [PeopleComponent, ProductsComponent],
   imports: [
     CommonModule,
     MainRoutingModule
